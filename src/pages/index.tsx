28a5bd9fb2899df8a3a3aec3b86aaf194ede4658
Kazuki1131 <onetsubyo@gmail.com>
2022-06-11 20:48:52 +0900

    feat: add nav component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,8 @@
-import type { NextPage } from 'next'
 import Head from 'next/head'
 import MainLayout from '../layouts'
 import styles from '../styles/Home.module.scss'
 import Article from '../components/article'
+import Nav from "../components/nav";
 
 const Home = (props) => {
   return (
@@ -10,9 +10,16 @@ const Home = (props) => {
 			<Head>
 				<title>Simple News</title>
 			</Head>
-
-			<div className={styles.main}>
-				<Article title="headlines" articles={props.topArticles} />
+			<div className={styles.contents}>
+				<div className={styles.nav}>
+					<nav>
+						<Nav />
+					</nav>
+				</div>
+				<div className={styles.blank} />
+				<div className={styles.main}>
+					<Article title="headline" articles={props.topArticles} />
+				</div>
 			</div>
 		</MainLayout>
 	);
